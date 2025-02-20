80992d1deb7236cb0cf756b248c6e87949ef19a4
Daniel Rodrigues de Santana <daniel.desantana@castgroup.com.br>
2025-02-20 15:41:57 -0300

    Exemplo de Formulario

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,31 @@
 <template>
-  <div class="card flex flex-col gap-2">
-    <div class="font-semibold text-xl">Form Sample</div>
-    <div class="pl-4">
-      <FloatLabel variant="in">
-        <InputText id="in_label" v-model="txtInput" variant="filled" />
-        <label for="in_label">In Label</label>
-      </FloatLabel>
+  <div class="flex flex-col flex-wrap gap-2 w-48">
+    <div class="font-semibold text-xl pl-4">
+      <h2>Form Sample</h2>
     </div>
     <div class="pl-4">
-      <Select v-model="selectedCity" :options="cities" optionLabel="name" placeholder="Select a City" />
+      <InputText id="in_label" placeholder="Enter e-mail" v-model="txtInput" variant="filled" size="small"  />
     </div>
     <div class="pl-4">
-      <Button label="Ok" icon="pi pi-check" @click="showSelected()"></Button>
+      <Select v-model="selectedCity" :options="cities" optionLabel="name" placeholder="Select a City" size="small" />
+    </div>
+    <div class="pl-4">
+      <Button label="Ok" icon="pi pi-check" size="small" @click="showSelected()"></Button>
     </div>
     <div class="pl-4" v-if="selectedCity && show">
-      You selected: {{ selectedCity }}
+      You selected: {{ formvalues }}
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-
 const show = ref(false)
 const txtInput = ref('')
 const selectedCity = ref()
+const formvalues = ref({
+  email: '',
+  codeCity: ''
+})
 const cities = ref([
   { name: 'New York', code: 'NY' },
   { name: 'Rome', code: 'RM' },
@@ -34,5 +36,10 @@ const cities = ref([
 
 const showSelected = () => {
   show.value = true
+
+  formvalues.value = {
+    email: txtInput.value,
+    codeCity: selectedCity.value.code
+  }
 }
 </script>
